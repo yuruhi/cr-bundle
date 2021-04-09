344ce7ec0b5254facb00505c85cf80aa9bab0a68
yuruhi <yuruhiya@gmail.com>
2021-04-09 18:21:26 +0900

    add spec

--- spec/cr-bundle_spec.cr
+++ spec/cr-bundle_spec.cr
@@ -1,9 +1,45 @@
 require "./spec_helper"
+require "file_utils"
 
-describe Cr::Bundle do
-  # TODO: Write tests
+describe CrBundle do
+  describe "bundler" do
+    it "standard" do
+      File.write("a.cr", %[puts "a.cr"])
+      File.write("b.cr", %[require "./a"\nputs "b.cr"])
+      run_bundle("b.cr").should eq <<-RESULT
+      # require "./a"
+      puts "a.cr"
+      puts "b.cr"
+      RESULT
+      FileUtils.rm(%w[a.cr b.cr])
+    end
+    it "require same file" do
+      File.write("a.cr", %[puts "a.cr"])
+      File.write("b1.cr", %[require "./a"\nputs "b1.cr"])
+      File.write("b2.cr", %[require "./a"\nputs "b2.cr"])
+      File.write("c.cr", %[require "./b1"\nrequire "./b2"\nputs "c.cr"])
+      run_bundle("c.cr").should eq <<-RESULT
+      # require "./b1"
+      # require "./a"
+      puts "a.cr"
+      puts "b1.cr"
+      # require "./b2"
+      
+      puts "b2.cr"
+      puts "c.cr"
+      RESULT
+      FileUtils.rm(%w[a.cr b1.cr b2.cr c.cr])
+    end
+    it "require each other" do
+      File.write("a.cr", %[require "./b"\nputs "a"])
+      File.write("b.cr", %[require "./a"\nputs "b"])
+      run_bundle("a.cr").should eq <<-RESULT
+      # require "./b"
 
-  it "works" do
-    false.should eq(true)
+      puts "b"
+      puts "a"
+      RESULT
+      FileUtils.rm(%w[a.cr b.cr])
+    end
   end
 end
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,7 @@
 require "spec"
 require "../src/cr-bundle"
+
+def run_bundle(file)
+  bundler = CrBundle::Bundler.new(CrBundle::Options.new)
+  bundler.bundle(File.read(file), Path[file].expand)
+end
